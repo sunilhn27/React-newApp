f5cc4349678b6688aea414c5cc20639cc04d7977
Sunil H N <31463813+suniljackie@users.noreply.github.com>
2022-09-09 21:55:56 +0530

    changes in news File

---
+++ News.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class News extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello Sunil</h1>
+      </div>
+    )
+  }
+}

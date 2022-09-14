c121f6dd52c87736cbf3795aafcafd7a4bbfbfdc
Sunil H N <sunilh.n27@gmail.com>
2022-09-14 22:11:58 +0530

    fixed

--- src/App.js
+++ src/App.js
@@ -11,19 +11,19 @@ function App() {
       <header className="App-header">
         <Navbar />
         <Routes>
-          <Route exact path="/general" key="general" element={<News pageSize={5} country="in" category="general" />} />
-          <Route exact path="/business" key="business" element={<News pageSize={8} country="in" category="business" />} />
-          <Route exact path="/entertainment" key="entertainment" element={<News pageSize={5} country="in" category="entertainment" />} />
-          <Route exact path="/health" key="health" element={<News pageSize={5} country="in" category="health" />} />
-          <Route exact path="/science" key="science" element={<News pageSize={5} country="in" category="science" />} />
-          <Route exact path="/sports" key="sports" element={<News pageSize={5} country="in" category="sports" />} />
-          <Route exact path="/technology" key="technology" element={<News pageSize={5} country="in" category="technology" />} />
+          <Route exact path="/general" element={<News key="general" pageSize={8} country="in" category="general" />} />
+          <Route exact path="/business" element={<News key="business" pageSize={8} country="in" category="business" />} />
+          <Route exact path="/entertainment" element={<News key="entertainment" pageSize={8} country="in" category="entertainment" />} />
+          <Route exact path="/health" element={<News pageSize={8} key="health" country="in" category="health" />} />
+          <Route exact path="/science" element={<News pageSize={8} key="science" country="in" category="science" />} />
+          <Route exact path="/sports" element={<News pageSize={8} key="sports" country="in" category="sports" />} />
+          <Route exact path="/technology" element={<News pageSize={8} key="technology" country="in" category="technology" />} />
         </Routes>
         <Footer />
       </header>
     </div>
   );
 }
-//pageSize={5}
+//pageSize={8}
 
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ export default class Navbar extends Component {
             <div>
                 <nav className="navbar navbar-expand-lg bg-dark">
                     <div className="container-fluid">
-                        <Link className="navbar-brand text-light" to="/">Navbar</Link>
+                        <Link className="navbar-brand text-light" to="/general">Navbar</Link>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
